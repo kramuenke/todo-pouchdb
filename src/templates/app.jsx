b1c68bc38e8959812d3e8f0d049623abecec1aa4
Kai Ramuenke <kai.ramuenke@gmail.com>
2014-05-09 15:43:27 +1000

    refresh list on changes

--- src/templates/app.jsx
+++ src/templates/app.jsx
@@ -5,6 +5,13 @@
   var ENTER_KEY = 13
 
   var pouch = new PouchDB('todos');
+  var remoteCouch = 'http://localhost:5984/todos';
+
+  function sync() {
+    var opts = {live: true};
+    pouch.replicate.to(remoteCouch, opts);
+    pouch.replicate.from(remoteCouch, opts);
+  }
 
   var NewTodo = React.createClass({
     newTodoCreated: function(event) {
@@ -28,23 +35,35 @@
     render: function() {
       var todos = [];
       this.props.todos.forEach(function(todo) {
-        todos.push(<li>{todo.title}</li>);
+        todos.push(<li key={todo.title}>{todo.title}</li>);
       });
       return (<ul>{todos}</ul>);
     }
   });
 
   var TodoApp = React.createClass({
+
     getInitialState: function() {
       return { todos: [] };
     },
 
     componentDidMount: function() {
-      pouch.allDocs({ include_docs: true, descending: true}, function(err, doc) {
+      var updateState = function(err,doc) {
         this.setState( {
           todos: _.map(doc.rows, function(row) { return row.doc; })
         });
+      }
+
+      pouch.info(function(err, info) {
+        pouch.changes({
+          since: info.update_seq,
+          live: true
+        }).on('change', function() {
+          pouch.allDocs({ include_docs: true, descending: true}, updateState.bind(this));
+        }.bind(this));
       }.bind(this));
+
+      pouch.allDocs({ include_docs: true, descending: true}, updateState.bind(this));
     },
 
     newTodo: function(todo) {
@@ -61,13 +80,15 @@
     render: function() {
       return (
         <div>
-          <h1>Your Todos</h1>
+          <h1>My Todos</h1>
           <NewTodo newTodo={this.newTodo}/>
           <TodoList todos={this.state.todos}/>
         </div>
       );
     }
   });
+
+  sync();
 
   React.renderComponent(
     <TodoApp/>,
